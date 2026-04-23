25fcfc3144231ce149849517e2976de3e81b087a
Syam Naidu <mac@macs-MBP.lan>
2026-04-23 09:38:36 +0530

    Day 1 complete - Firebase integrated, app running on Android

--- constants/appwrite.ts
+++ constants/appwrite.ts
@@ -1,16 +1,24 @@
-// Appwrite client configuration for React Native/Expo
-import { Client, Account } from 'appwrite';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
 
-const appwrite = {
-  client: new Client(),
-  account: null as Account | null,
+const firebaseConfig = {
+  apiKey: "AIzaSyCHpsKvz5kc_ANFtfhf-UoAV5-gbiZa-lI",
+  authDomain: "nextgen-dating-app.firebaseapp.com",
+  projectId: "nextgen-dating-app",
+  storageBucket: "nextgen-dating-app.firebasestorage.app",
+  messagingSenderId: "283687342429",
+  appId: "1:283687342429:web:edfb06c051f4db9b6298b4",
+  databaseURL: "https://nextgen-dating-app-default-rtdb.firebaseio.com",
 };
 
-// TODO: Replace these with your Appwrite endpoint and project ID
-appwrite.client
-  .setEndpoint('https://YOUR_APPWRITE_ENDPOINT/v1') // e.g., 'https://cloud.appwrite.io/v1'
-  .setProject('YOUR_PROJECT_ID');
+const app = initializeApp(firebaseConfig);
 
-appwrite.account = new Account(appwrite.client);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const rtdb = getDatabase(app);
+export const storage = getStorage(app);
 
-export default appwrite;
+export default app;
